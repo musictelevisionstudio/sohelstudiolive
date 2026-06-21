b9b79cc9ae4fcecfb94ad35ae2883caf0dfb6ba3
musictelevisionstudio <musictelevisionstudio@gmail.com>
2026-06-21 23:33:49 +0600

    Configure database connection using environment variables

--- config/db.php
+++ config/db.php
@@ -1,1 +1,14 @@
+<?php
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// কানেকশন ঠিক থাকলে কোনো আউটপুট আসবে না, সেটিই স্বাভাবিক
+?>
 
